16dc24abb04656943bf8f910041b17529ba0890b
tyguy3 <tyguy3@b1f42851-f8d9-dcd7-fd08-d182dfaba04f>
2011-07-21 00:21:31 +0000

    Updated Final Project to reflect recent changes.

--- branches/Final Project/Assets/Scripts/Networking/InstantiateCollectible(s).js
+++ branches/Final Project/Assets/Scripts/Networking/InstantiateCollectible(s).js
@@ -1,7 +1,16 @@
+// Team: SkyBox Studios
+
+// Team Members:
+// Nathan Bailey
+// Steven Bass
+// Tyler Cochran
+// Adil Delawalla
+// Tyler Meehan
+
 var Collectible : Transform;
 
 function OnNetworkLoadedLevel() {
-	//Instantiating player
+	//Instantiating collectible
 	if(Network.isServer){
 		Network.Instantiate(Collectible, transform.position, transform.rotation, 0);	
 	}
