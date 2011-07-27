50454b52ea7d11a2e1c177ef5a330cfd22d5db24
pckl300 <pckl300@gmail.com>
2011-07-27 21:06:25 +0000

    fixing sounds

--- branches/Final Project/Assets/Scripts/Environment/PowerUpController.js
+++ branches/Final Project/Assets/Scripts/Environment/PowerUpController.js
@@ -2,6 +2,8 @@ public var Powerups : GameObject[];
 var respawnTime : float = 1.5;
 private var Timers : float[];
 private var previousXPositions : int[];
+private var played : boolean = false;
+var pickupSound : AudioSource;
 
 function Awake(){
 	if(Network.isServer){
@@ -18,9 +20,14 @@ function Update(){
 	if(Network.isServer){
 		for(var i = 0; i < Powerups.length; i++){
 			if(Powerups[i].transform.position.x >= 10000){
+				if (!played){
+					networkView.RPC("RPCPlayPickupSound", RPCMode.All);
+					played = true;
+				}
 				Timers[i] += Time.deltaTime;
 				if(Timers[i] >= respawnTime){
 					Timers[i] = 0;
+					played = false;
 					networkView.RPC("moveOneBack", RPCMode.AllBuffered, i);
 				}
 			}
@@ -31,4 +38,10 @@ function Update(){
 @RPC
 function moveOneBack(i : int){
 	Powerups[i].transform.position.x = previousXPositions[i];
+}
+
+@RPC
+function RPCPlayPickupSound()
+{
+	pickupSound.Play();
 }
