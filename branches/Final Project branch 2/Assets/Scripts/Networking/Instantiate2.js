722efb923cbb92d3ba8c8cad4968865f332d0854
nathanaelbailey@gmail.com <nathanaelbailey@gmail.com@b1f42851-f8d9-dcd7-fd08-d182dfaba04f>
2011-07-28 05:06:45 +0000

    Shadows...

--- branches/Final Project branch 2/Assets/Scripts/Networking/Instantiate2.js
+++ branches/Final Project branch 2/Assets/Scripts/Networking/Instantiate2.js
@@ -8,12 +8,10 @@
 // Tyler Meehan
 
 var PlayerPrefab : Transform;
-var ShadowProjector : Transform;
 
 function OnNetworkLoadedLevel() {
 	//Instantiating player
 	if(Network.isClient){
-		ShadowProjector = PlayerPrefab.Find("Blob Shadow Projector");
 		Network.Instantiate(PlayerPrefab, transform.position, transform.rotation, 0);	
 	}
 }
@@ -21,5 +19,4 @@ function OnNetworkLoadedLevel() {
 function OnPlayerDisconnected (player : NetworkPlayer) {
 	Network.RemoveRPCs(player,0);
 	Network.DestroyPlayerObjects(player);
-	Destroy(ShadowProjector);
 }
