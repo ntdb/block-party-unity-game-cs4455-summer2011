5f5ce2adad9ffd80dc335dcda4c1e9442285841e
pckl300 <pckl300@gmail.com>
2011-07-28 00:24:10 +0000

    fixing scripts

--- branches/Final Project/Assets/Scripts/Networking/SyncHelper.js
+++ branches/Final Project/Assets/Scripts/Networking/SyncHelper.js
@@ -1,14 +1,25 @@
 var Wings : GameObject;
 var Skates : GameObject;
+private var coloration : Color = Color(.1451, .3843, .7255, 1);
 
 function switchMyWings(me : NetworkViewID, activating : boolean){
 	if(networkView.isMine)
 		networkView.RPC("RPCSwitchWings", RPCMode.OthersBuffered, me, activating);
 }
 
+function switchMyHeavy(me : NetworkViewID, activating : boolean){
+	if(networkView.isMine)
+		networkView.RPC("RPCSwitchHeavy", RPCMode.OthersBuffered, me, activating);
+}
+
 function getWings(me : NetworkViewID){
 	if(networkView.isMine)
 		networkView.RPC("RPCGetWings", RPCMode.OthersBuffered, me);
+}
+
+function getHeavy(me : NetworkViewID){
+	if(networkView.isMine)
+		networkView.RPC("RPCGetHeavy", RPCMode.OthersBuffered, me);
 }
 
 @RPC
@@ -27,9 +38,26 @@ function RPCSwitchWings(player : NetworkViewID, activating : boolean){
 }
 
 @RPC
+function RPCSwitchHeavy(player : NetworkViewID, activating : boolean){
+	var other : Transform = NetworkView.Find(player).transform;
+	if (activating){
+		other.gameObject.renderer.material.color = Color.gray;
+	} else {
+		other.gameObject.renderer.material.color = coloration;
+	}
+}
+
+@RPC
 function RPCGetWings(player : NetworkViewID){
 	var other : Transform = NetworkView.Find(player).transform;
+	other.gameObject.renderer.material.color = coloration;
 	other.rigidbody.rotation = Quaternion.identity;
 	var wings : GameObject = Instantiate(Wings, Vector3(other.rigidbody.position.x, other.rigidbody.position.y - 0.5, other.rigidbody.position.z), other.rigidbody.rotation);
 	wings.transform.parent = other;
+}
+
+@RPC
+function RPCGetHeavy(player : NetworkViewID){
+	var other : Transform = NetworkView.Find(player).transform;
+	other.GetChild(0).GetComponent("WingsController").DeactivateWings();
 }
