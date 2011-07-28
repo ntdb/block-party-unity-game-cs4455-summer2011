283e87d2b02d6997c3cad5ad0f09f610bc2bcd88
nathanaelbailey@gmail.com <nathanaelbailey@gmail.com@b1f42851-f8d9-dcd7-fd08-d182dfaba04f>
2011-07-28 04:32:47 +0000

    Shadow!

---
+++ branches/Final Project branch 2/Assets/Scripts/BlobProjectorFollow.js
@@ -0,0 +1,12 @@
+var target : Transform;
+var height = 10;
+
+function Awake() {
+	transform.parent = null;
+}
+
+function Update () {
+	transform.position.x = target.position.x;
+	transform.position.y = target.position.y + height;
+	transform.position.z = target.position.z;
+}
